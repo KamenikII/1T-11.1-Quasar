b647a354b3418b3f2157b635ad87f2203bfa7461
Ilya <107436210+KamenikII@users.noreply.github.com>
2024-08-06 13:07:47 +0300

    Add page: About, Help, Contacts

---
+++ src/pages/Contacts.vue
@@ -0,0 +1,10 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5>Contacts</h5>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur fugit quam laboriosam ab aut consequuntur!</p>
+  </q-page>
+</template>
+
+<script>
+
+</script>
